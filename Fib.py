010122e252d09aefb2604dea42070eae54abee8b
m3l1k4 <melikaihelas@gmail.com>
2021-01-14 17:51:15 -0800

    Create Fib.py

---
+++ Fib.py
@@ -0,0 +1,48 @@
+# Write a function that calculates the 
+# nth Fibonacci number in O(n) 
+# time or better without using any "for"
+#  or "while" loops. (Feel free to use the space 
+#  below or submit a link to your work.)
+
+
+import sys
+
+NUM=8
+
+def fib(NUM):
+    if NUM <= 1:
+        return n
+     return fib(n-1) + fib(n-2)
+
+
+def main():
+
+
+
+
+
+
+
+
+# Write an immutable function that merges the following inputs into a single list. (Feel free to use the space below or submit a link to your work.)
+
+# Inputs
+# - Original list of strings
+# - List of strings to be added
+# - List of strings to be removed
+
+# Return
+# - List shall only contain unique values
+# - List shall be ordered as follows
+# --- Most character count to least character count
+# --- In the event of a tie, reverse alphabetical
+
+# Other Notes
+# - You can use any programming language you like
+# - The function you submit shall be runnable
+
+# For example:
+
+# Original List = ['one', 'two', 'three',]
+# Add List = ['one', 'two', 'five', 'six]
+# Delete List = ['two', 'five']
